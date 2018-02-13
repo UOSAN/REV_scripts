aadc251fe0aa9a95ee985bc92d93278a328b2647
kdestasio <kdestasio@gmail.com>
2018-02-13 13:31:27 -0800

    test file name splitting

--- org/dcm2bids/bidsQC.py
+++ org/dcm2bids/bidsQC.py
@@ -51,13 +51,13 @@ if not os.path.isfile(errorlog):
 for dirpath, subdirs, files in os.walk(tempdir):
 # For each sequence type in the subdirectory
 	for dirname in subdirs:
-		wave=print(dirname.split("_")[1])
-	for file in files:
-		sequenceNumber=file.split("_")[0]
-		subject=file.split("_")[1]
-		sequenceName=file.split("_")[3]
-		with open(outputlog, 'a') as logfile:
-			logfile.write(subject+"-"+wave+"_"+sequenceNumber+"-"+sequenceName+os.linesep)
+		wave=dirname.split("_")[1]
+		for file in files:
+			sequenceNumber=file.split("_")[0]
+			subject=file.split("_")[1]
+			sequenceName=file.split("_")[3]
+			with open(outputlog, 'a') as logfile:
+				logfile.write(subject+"-"+wave+"_"+sequenceNumber+"-"+sequenceName+os.linesep)
 	# If there are duplicates of any sequences of interest (task, anat, fmap)
 	# Then copy the largest of those files to that participant's BIDS directory
 	# Rename that file with BIDS format
