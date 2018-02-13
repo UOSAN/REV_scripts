cebcff5ceb639fbef022b636107535b5ef7e329a
kdestasio <kdestasio@gmail.com>
2018-02-13 12:33:28 -0800

    make a log directory for log files

--- org/dcm2bids/dcm2bids_batch.py
+++ org/dcm2bids/dcm2bids_batch.py
@@ -27,17 +27,16 @@ group="sanlab"
 study="REV"
 
 # Set directories
-currentdir=os.getcwd()
+logdir=os.getcwd()+"/logs"
 dicomdir="/projects/" + group + "/shared/DICOMS/" + study
 archivedir="/projects/" + group + "/shared/" + study + "/archive"
 niidir=archivedir + "/clean_nii"
-
-outputlog=currentdir + "/outputlog_nii2bids.txt"
-errorlog=currentdir + "/errorlog_nii2bids.txt"
-
 codedir= "/projects/" + group + "/shared/" + study + "/" + study + "_scripts/org/dcm2bids/" # Contains subject_list.txt, config file, and dcm2bids_batch.py
 configfile= codedir + study + "_config.json" # path to and name of config file
 image= "/projects/" + group + "/shared/containers/Dcm2Bids-master.simg"
+
+outputlog=logdir + "/outputlog_nii2bids.txt"
+errorlog=logdir + "/errorlog_nii2bids.txt"
 
 # Source the subject list (needs to be in your current working directory)
 subjectlist="subject_list.txt" 
@@ -52,12 +51,6 @@ def touch(path):
 	with open(path, 'a'):
 		os.utime(path, None)
 
-## Check/create log files
-if not os.path.isfile(outputlog):
-	touch(outputlog)
-if not os.path.isfile(errorlog):
-	touch(errorlog)
-
 # Check directory dependencies
 if not os.path.isdir(dicomdir):
 	print("Incorrect dicom directory specified")
@@ -65,8 +58,14 @@ if not os.path.isdir(niidir):
 	print("Incorrect nifti directory specified")
 if not os.path.isdir(archivedir):
 	print("Incorrect archive directory specified")
-if not os.path.isdir(niidir + "/logs"):
-	os.mkdir(niidir + "/logs")
+if not os.path.isdir(logdir):
+	os.mkdir(logdir)
+
+## Check/create log files
+if not os.path.isfile(outputlog):
+	touch(outputlog)
+if not os.path.isfile(errorlog):
+	touch(errorlog)
 
 ##################################
 # DICOM To BIDS Conversion
@@ -87,7 +86,7 @@ for line in lines:
 		with open(outputlog, 'a') as logfile:
 			logfile.write(subjectdir+os.linesep)
 		# Create a job to submit to the HPC with sbatch 
-		batch_cmd = 'sbatch --job-name dcm2bids_{subjectdir} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {niidir}/logs/{subjectdir}_dcm2bids_output.txt -e {niidir}/logs/{subjectdir}_dcm2bids_error.txt --wrap="singularity run -B {dicomdir} -B {niidir} -B {codedir} {image} -d {subjectpath} -s {wave} -p {subject} -c {configfile} -o {niidir}"'.format(subjectdir=subjectdir,dicomdir=dicomdir,wave=wave,codedir=codedir,configfile=configfile,subject=subject,niidir=niidir,subjectpath=subjectpath,group=group,image=image)
+		batch_cmd = 'sbatch --job-name dcm2bids_{subjectdir} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {logdir}/{subjectdir}_dcm2bids_output.txt -e {logdir}/{subjectdir}_dcm2bids_error.txt --wrap="singularity run -B {dicomdir} -B {niidir} -B {codedir} {image} -d {subjectpath} -s {wave} -p {subject} -c {configfile} -o {niidir}"'.format(logdir=logdir,subjectdir=subjectdir,dicomdir=dicomdir,wave=wave,codedir=codedir,configfile=configfile,subject=subject,niidir=niidir,subjectpath=subjectpath,group=group,image=image)
 		# Submit the job
 		subprocess.call([batch_cmd], shell=True)
 	else:
