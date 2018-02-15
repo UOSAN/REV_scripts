3364e8712f320159626d76bf8993857a6992f99c
kdestasio <kdestasio@gmail.com>
2018-02-15 09:32:04 -0800

    Test os.walk through temp dirs.

--- org/dcm2bids/bidsQC.py
+++ org/dcm2bids/bidsQC.py
@@ -70,13 +70,14 @@ if not os.path.isfile(errorlog):
 for dirpath, dirnames, files in os.walk(niidir):
 # For each sequence type in the subdirectory
 	for dirname in dirnames:
-		subdir = glob.glob(dirname + "/sub-REV*")
-		fmap = glob.glob(subdir + "/fmap")
-		anat = glob.glob(subdir + "/anat")
-		func = glob.glob(subdir + "/func")
-		print("fmap = " + fmap)
-		print("anat = " + anat)
-		print("func = " + func)
+		subdirs = glob.glob(dirname + "/sub-REV*")
+		for subdir in subdirs:
+			fmap = glob.glob(subdir + "/fmap")
+			anat = glob.glob(subdir + "/anat")
+			func = glob.glob(subdir + "/func")
+			print("fmap = " + fmap)
+			print("anat = " + anat)
+			print("func = " + func)
 
 			#if file in files : # has run then
 		# retain the last run
