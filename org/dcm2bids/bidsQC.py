e4c4d1926fd7f343e7503d630a111ebf3cdf137e
kdestasio <kdestasio@gmail.com>
2018-02-15 12:21:48 -0800

    glob match run

--- org/dcm2bids/bidsQC.py
+++ org/dcm2bids/bidsQC.py
@@ -79,8 +79,8 @@ for dirpath, dirnames, files in os.walk(niidir):
 # check for run string in correct place
 			for file in os.listdir(fullpath):
 				if file.split("_")[0:2] != glob.glob("run-[0-9]{2}"):
-					if file.split("_")[-2] == glob.glob("run-[0-9]{2}"):
-						print(file.split("_")[-2]) 
+					print file.split("_")[-2] # == glob.glob("run-[0-9]{2}"):
+						#print(file.split("_")[-2]) 
 
 	#subdirs = glob.glob(dirpath + "/sub-REV*")
 	# for subdir in subdirs:
