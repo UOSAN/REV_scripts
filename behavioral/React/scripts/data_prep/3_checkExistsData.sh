ff59f90d34ef7522e17cad776c20c986f5f1a1a1
kdestasio <kdestasio@gmail.com>
2018-04-06 13:08:12 -0700

    make task variable

--- behavioral/React/scripts/data_prep/3_checkExistsData.sh
+++ behavioral/React/scripts/data_prep/3_checkExistsData.sh
@@ -7,13 +7,14 @@
 
 # Change this, path to data and script repos
 repopath="/Users/kristadestasio/Desktop"
+task="React"
 
 # Set variables
-datadir="$repopath/REV_scripts/behavioral/GNG/data" # Location of the data files to be analyzed
-logdir="$repopath/REV_scripts/behavioral/GNG/logs"
+datadir="$repopath/REV_scripts/behavioral/$task/data" # Location of the data files to be analyzed
+logdir="$repopath/REV_scripts/behavioral/$task/logs"
 errorlog="$logdir/errorlog_filecheck.txt"
 outputlog="$logdir/outputlog_filecheck.txt"
-sublist="$repopath/REV_scripts/behavioral/GNG/scripts/data_prep"
+sublist="$repopath/REV_scripts/behavioral/$task/scripts/data_prep"
 
 
 cd $repopath/REV_scripts/behavioral/GNG/scripts/data_prep
@@ -31,11 +32,11 @@ echo "--------------PRE DATA--------------" > $outputlog
 echo "--------------PRE DATA--------------" > $errorlog
 for sub in ${sublist[@]}; do
 	for run in $runnum; do
-		if [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -eq 1 ]; then
+		if [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -eq 1 ]; then
 		echo ${sub} "run" ${run} "exists" >> $outputlog
-		elif [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -gt 1 ]; then
+		elif [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -gt 1 ]; then
 			echo ${sub} "run" ${run} "duplicate" >> $errorlog
-		elif [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -eq 0 ]; then
+		elif [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -eq 0 ]; then
 			echo ${sub} "run" ${run} "missing" >> $errorlog
 		fi
 	done
@@ -48,11 +49,11 @@ echo "--------------POST DATA--------------" >> $outputlog
 echo "--------------POST DATA--------------" >> $errorlog
 for sub in ${sublist[@]}; do
 	for run in $runnum; do
-		if [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -eq 1 ]; then
+		if [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -eq 1 ]; then
 		echo ${sub} "run" ${run} "exists" >> $outputlog
-		elif [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -gt 1 ]; then
+		elif [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -gt 1 ]; then
 			echo ${sub} "run" ${run} "duplicate" >> $errorlog
-		elif [ $(ls "${datadir}"/"${sub}_GNG${run}".mat | wc -l) -eq 0 ]; then
+		elif [ $(ls "${datadir}"/"${sub}_REV_${task}${run}"*.mat | wc -l) -eq 0 ]; then
 			echo ${sub} "run" ${run} "missing" >> $errorlog
 		fi
 	done
