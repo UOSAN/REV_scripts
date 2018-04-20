0083c472c54dc0d6a3a2f2946443ae46422fccef
kdestasio <kdestasio@gmail.com>
2018-04-20 13:50:10 -0700

    make config file for dcm2bids_batch

---
+++ org/dcm2bids/config_bidsQC.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+
+# Set study info (change these for your study)
+group = "sanlab"
+study = "REV"
+
+# Set directories (Check these for your study)
+
+# logdir = os.getcwd() + "/logs_bidsQC"
+# bidsdir = "/projects/" + group + "/shared/" + study + "/bids_data_copy"
+# tempdir = bidsdir + "/tmp_dcm2bids"
+# outputlog = logdir + "/outputlog_bidsQC" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
+# errorlog = logdir + "/errorlog_bidsQC" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
+# derivatives = bidsdir + "/derivatives"
+
+# Set directories for local testing
+bidsdir = "/Users/kristadestasio/Desktop/bids_data"
+logdir = bidsdir + "/logs_bidsQC"
+tempdir = bidsdir + "/tmp_dcm2bids"
+outputlog = logdir + "/outputlog_bidsQC_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
+errorlog = logdir + "/errorlog_bidsQC_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
+derivatives = bidsdir + "/derivatives"
--- org/dcm2bids/config_dcm2bids_batch.py
+++ org/dcm2bids/config_dcm2bids_batch.py
@@ -7,13 +7,13 @@ group = "sanlab"
 study = "REV"
 
 gitrepo = "REV_scripts" # Parent folder that contains your org ("organization") and dcm2bids folders. If your directory structure is different, you'll have to adjust.
-dicomdir = os.path.join(os.sep, "projects", os.sep, "lcni", os.sep, "dcm", group, "Archive", study)
+dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", study)
 
 # Set directories
 niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
-codedir = os.path.join(os.sep, "projects", group, "shared", study, gitrepo, "org", os.sep, "dcm2bids")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
+codedir = os.path.join(os.sep, "projects", group, "shared", study, gitrepo, "org", "dcm2bids")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
 configfile = os.path.join(codedir, study + "_config.json")  # path to and name of config file
-image = os.path.join(os.sep, "projects", group, "shared", os.sep, "containers", os.sep, "Dcm2Bids-master.simg")
+image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
 logdir = os.path.join(codedir, "logs_dcm2bids")
 
 outputlog = os.path.join(logdir, "outputlog_dcmn2bids.txt")
