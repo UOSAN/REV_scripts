912c928daa80624750800fc011a7882509a8d281
kdestasio <kdestasio@gmail.com>
2018-04-29 12:28:31 -0700

    real sub list

--- org/dcm2bids/config_dcm2bids_batch.py
+++ org/dcm2bids/config_dcm2bids_batch.py
@@ -13,7 +13,7 @@ dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", stu
 # Set directories
 niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
 codedir = os.path.join(os.sep, "projects", group, "shared", study, gitrepo, "org", "dcm2bids")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-configfile = os.path.join(codedir, "study_config.json")  # path to and name of config file
+configfile = os.path.join(codedir, study + "_config.json")  # path to and name of config file
 image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
 logdir = os.path.join(codedir, "logs_dcm2bids")
 
