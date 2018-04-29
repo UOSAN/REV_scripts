8308ccaf4ad23366c9e5ae5b2e42b4e7b9467f0a
kdestasio <kdestasio@gmail.com>
2018-04-29 12:26:46 -0700

    give study_config file a generic name

--- org/dcm2bids/config_dcm2bids_batch.py
+++ org/dcm2bids/config_dcm2bids_batch.py
@@ -13,7 +13,7 @@ dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", stu
 # Set directories
 niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
 codedir = os.path.join(os.sep, "projects", group, "shared", study, gitrepo, "org", "dcm2bids")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-configfile = os.path.join(codedir, study + "_config.json")  # path to and name of config file
+configfile = os.path.join(codedir, "study_config.json")  # path to and name of config file
 image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
 logdir = os.path.join(codedir, "logs_dcm2bids")
 
@@ -21,4 +21,4 @@ outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime
 errorlog = os.path.join(logdir, "errorlog_dcm2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 
 # Source the subject list (needs to be in your current working directory)
-subjectlist = "subject_list_test.txt"
+subjectlist = "subject_list.txt"
