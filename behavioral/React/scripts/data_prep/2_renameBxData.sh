090556b22a6aa6150fb5f065c4707af7d7ed9033
mmoss <mmoss@pschitadmins-MacBook-Pro.local>
2018-05-30 16:53:18 -0700

    finished edits for renaming the React Bx data (from scanning)

--- behavioral/React/scripts/data_prep/2_renameBxData.sh
+++ behavioral/React/scripts/data_prep/2_renameBxData.sh
@@ -21,19 +21,10 @@ touch "${outputlog}"
 # Idiosyncratic file renaming to correct naming errors
 echo "-------------------Renaming $task files-------------------" > $outputlog
 cd $outputdir
-# MM: FIX THE BELOW CRAP: NEED TO CHECK INCORRECT NAMES MANUALLY --- mv current_filename target_filename
-# mv REV13_REV_GNG4.txt_04-May-2015_10-25.mat REV013_REV_GNG4.txt_04-May-2015_10-25.mat  
-# echo "REV13_REV_GNG4.txt_04-May-2015_10-25.mat REV013_REV_GNG4.txt_04-May-2015_10-25.mat" >> $outputlog
-# mv REV13_REV_GNG3.txt_04-May-2015_10-33.mat REV013_REV_GNG3.txt_04-May-2015_10-33.mat
-# echo "REV13_REV_GNG3.txt_04-May-2015_10-33.mat REV013_REV_GNG3.txt_04-May-2015_10-33.mat" >> $outputlog
-# mv 32_REV_GNG3.txt_10-Jul-2015_13-40.mat REV032_REV_GNG3.txt_10-Jul-2015_13-40.mat
-# echo "32_REV_GNG3.txt_10-Jul-2015_13-40.mat REV032_REV_GNG3.txt_10-Jul-2015_13-40.mat" >> $outputlog
-# mv 32_REV_GNG4.txt_10-Jul-2015_13-47.mat REV032_REV_GNG4.txt_10-Jul-2015_13-47.mat
-# echo "32_REV_GNG4.txt_10-Jul-2015_13-47.mat REV032_REV_GNG4.txt_10-Jul-2015_13-47.mat" >> $outputlog
-# mv 102_REV_GNG2.txt_19-Jan-2016_18-02.mat REV102_REV_GNG2.txt_19-Jan-2016_18-02.mat
-# echo "102_REV_GNG2.txt_19-Jan-2016_18-02.mat REV102_REV_GNG2.txt_19-Jan-2016_18-02.mat" >> $outputlog
-# mv 102_REV_GNG1.txt_19-Jan-2016_17-53.mat REV102_REV_GNG1.txt_19-Jan-2016_17-53.mat
-# echo "102_REV_GNG1.txt_19-Jan-2016_17-53.mat REV102_REV_GNG1.txt_19-Jan-2016_17-53.mat" >> $outputlog
+ mv REV057_REV_React2.txt_25-Aug-2015_18-15.mat REV057_REV_React1.txt_25-Aug-2015_18-15.mat   
+ echo "REV057_REV_React2.txt_25-Aug-2015_18-15.mat REV057_REV_React1.txt_25-Aug-2015_18-15.mat" >> $outputlog
+ mv REV027_REV_React4.txt_12-Jun-2015_11-18.mat REV027_REV_React3.txt_12-Jun-2015_11-18.mat
+ echo "REV027_REV_React4.txt_12-Jun-2015_11-18.mat REV027_REV_React3.txt_12-Jun-2015_11-18.mat" >> $outputlog
 
 # Rename all task files to format ID_run
 for file in $(ls *.mat)
@@ -47,4 +38,22 @@ for file in $(ls *.mat)
 		new=$(echo "$file" | sed -E 's/.{22}\.mat/.mat/') #22 indicates index (check how things are indexed here)
 		mv $file $new
 done
+
+mv REV12_React1.mat REV012_React1.mat
+echo "mv REV12_React1.mat REV012_React1.mat" >> $outputlog
+mv REV12_React2.mat REV012_React2.mat
+echo "mv REV12_React2.mat REV012_React2.mat" >> $outputlog
+mv 102_React1.mat REV102_React1.mat
+echo "mv 102_React1.mat REV102_React1.mat" >> $outputlog
+mv 102_React2.mat REV102_React2.mat
+echo "mv 102_React2.mat REV102_React2.mat" >> $outputlog
+mv REV13_React3.mat REV013_React3.mat
+echo "mv REV13_React3.mat REV013_React3.mat" >> $outputlog
+mv REV13_React4.mat REV013_React4.mat
+echo "mv REV13_React4.mat REV013_React4.mat" >> $outputlog
+mv 32_React3.mat REV032_React3.mat
+echo "mv 32_React3.mat REV032_React3.mat" >> $outputlog
+mv 32_React4.mat REV032_React4.mat
+echo "mv 32_React4.mat REV032_React4.mat" >> $outputlog
+
 echo "Done renaming files"
