ec67f778a12e6c108ecb4168bd88bd57dd363150
brendanhcullen <bcullen@uoregon.edu>
2018-08-03 11:00:34 -0700

    started writing fixFileSizes.sh script

---
+++ org/dcm2bids/fixFileSizes.sh
@@ -0,0 +1,24 @@
+# Replace wrong-sized bids_data files with correct-sized tmp files
+
+# Get list of full file paths for tmp files and bids files
+
+repopath="/Users/brendancullen/Desktop/REV/REV_scripts/org/dcm2bids"
+bids_paths="$repopath/file_size_bids_paths.txt"
+tmp_paths="$repopath/file_size_tmp_paths.txt"
+bids_files="$repopath/file_size_bids_files.txt"
+tmp_files="$repopath/file_size_tmp_files.txt"
+
+
+# Move all the files to a 'purgatory' folder 
+## for each file in bids_files, move file to new directory called "purgatory"
+
+# For each file in purgatory, chop the file name (using string split) to exlcude the run number and add wild card, e.g. REV003_ses-wave1_task-gng_acq-1*.nii.gz
+## save a list of these wild card names
+
+# cd into bids_data folder 
+
+# for each wild card name, search recursively through bids_data folder, get the bids_data file name, replace the file in purgatory folder with this file name, then move the bids_data file into `tmp_dcm2bids`
+
+# for each re-named purgatory file, move the file to bids_path folder
+
+
