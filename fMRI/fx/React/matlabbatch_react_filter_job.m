bf858c210b19973ca4c2713ed91eec07e20058f2
brendanhcullen <bcullen@uoregon.edu>
2018-08-14 13:48:58 -0700

    updated matlabbatch_react_filter with correct sequence of modules and dependencies

---
+++ fMRI/fx/React/matlabbatch_react_filter_job.m
@@ -0,0 +1,8 @@
+%-----------------------------------------------------------------------
+% Job saved on 10-Aug-2018 15:29:47 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/Users/brendancullen/Desktop/sub-REV001_React_data'};
+matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.filter = 'sub-REV*_React_data/sub-REV*_ses-wave1_task-react_acq-*_run-*_bold_space-MNI152NLin2009cAsym_preproc.nii.gz';
+matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPList';
