cca4f6f696ff9304fa6f82a94b129a6b840a4b4d
Krista DeStasio <kdestasio@gmail.com>
2018-08-23 15:20:37 -0700

    print correct task

--- behavioral/scripts/0_runscript.sh
+++ behavioral/scripts/0_runscript.sh
@@ -9,11 +9,11 @@ user="kristadestasio"
 path_to_scripts="/Users/${user}/Desktop/REV_scripts/behavioral/scripts" 
 #"/Users/mmoss/Dropbox/AH_Grad_Stuff/SAP/REV_scripts/behavioral/React/scripts/data_prep"
 
-echo "Copying React behavioral files"
+echo "Copying ${task} behavioral files"
 sh ${path_to_scripts}/1_copyBxData.sh
 
-echo "Renaming React behavioral files"
+echo "Renaming ${task} behavioral files"
 sh ${path_to_scripts}/2_rename_${task}_BxData.sh
 
-echo "Checking React behavioral files"
+echo "Checking ${task} behavioral files"
 sh ${path_to_scripts}/3_checkExistsData.sh
