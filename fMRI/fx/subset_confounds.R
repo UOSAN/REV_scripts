1b1ba05b2268ddacd7a303af2d440c54aa404a95
brendanhcullen <bcullen@uoregon.edu>
2018-08-28 12:01:29 -0700

    select only FramewiseDisplacement var and change output directory to a motion folder within derivatives folder

--- fMRI/fx/subset_confounds.R
+++ fMRI/fx/subset_confounds.R
@@ -6,7 +6,7 @@ setwd(working_dir)
 
 # Set output directory
 #output_dir = "/Users/brendancullen/Desktop/motion/"
-output_dir = "/projects/sanlab/shared/REV/REV_scripts/fMRI/ppc/motion/"
+output_dir = "/projects/sanlab/shared/REV/bids_data/derivatives/motion"
 
 # Get a list of all the confounds.tsv files
 confound_pattern = "*confounds*.tsv"
@@ -20,13 +20,13 @@ for (path in confounds_paths) {
     # Import the confounds.tsv file
     confounds_file <- rio::import(path)
     # Select only the motion paramters of interest
-    new_confounds_file <- dplyr::select(confounds_file, c(X, Y, Z, RotX, RotY, RotZ, stdDVARS, FramewiseDisplacement)) %>%
-    # new_confounds_file <- dplyr::select(confounds_file, c(stdDVARS, FramewiseDisplacement)) %>% # only select 2 summary vars r
+    #new_confounds_file <- dplyr::select(confounds_file, c(X, Y, Z, RotX, RotY, RotZ, stdDVARS, FramewiseDisplacement)) %>%
+    new_confounds_file <- dplyr::select(confounds_file, c(FramewiseDisplacement)) %>% # only select Framewise Displacement
       #convert NA's in FramewiseDisplacement and stdVARS to 0's
       mutate(FramewiseDisplacement = (as.numeric(ifelse(FramewiseDisplacement %in% "n/a", NA, FramewiseDisplacement)))) %>%
-      mutate(FramewiseDisplacement = ifelse(is.na(FramewiseDisplacement), 0, FramewiseDisplacement)) %>%
-      mutate(stdDVARS = (as.numeric(ifelse(stdDVARS %in% "n/a", NA, stdDVARS)))) %>%
-      mutate(stdDVARS = ifelse(is.na(stdDVARS), 0, stdDVARS))
+      mutate(FramewiseDisplacement = ifelse(is.na(FramewiseDisplacement), 0, FramewiseDisplacement))
+      #mutate(stdDVARS = (as.numeric(ifelse(stdDVARS %in% "n/a", NA, stdDVARS)))) %>%
+      #mutate(stdDVARS = ifelse(is.na(stdDVARS), 0, stdDVARS))
     # export to .txt
     new_file_name <- paste0(substr(path, 1, nchar(path) - 3), "txt") # change .tsv extension to .txt
     new_file_path <- paste0(output_dir, strsplit(new_file_name, "func/")[[1]][2]) # extract just the file name from the path name and join it with a new path name pointing to a "motion" directory
