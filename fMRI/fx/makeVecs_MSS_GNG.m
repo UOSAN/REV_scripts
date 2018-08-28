644fb8d79cbee5599803adea66bacfa0855ccbc0
Krista DeStasio <kdestasio@gmail.com>
2018-08-27 18:18:23 -0700

    use repodir variable for fx path

--- fMRI/fx/makeVecs_MSS_GNG.m
+++ fMRI/fx/makeVecs_MSS_GNG.m
@@ -4,6 +4,7 @@ lastSub = 144;
 exclude = []%[4 5 7 8 12 14 15 25 28 30 33 40 42 45 61 63 64 66 71 72 79 81 83 85 87 92 95 96 99 101 103 105 106 112 113 120 122 123 125 128 132 133 139 143]; % If you want to exclude any numbers, put them in this vector (e.g. exclude = [5 20];)
 task = 'GNG';
 runs = [1 2 3 4];
+repodir = ['~/Desktop/REV/REV_BxData/']; %edit this path for your local computer
 dataFolder = ['~/Desktop/REV_BxData/data/' task];
 %dataFolder = ['/projects/sanlab/shared/REV/REV_scripts/behavioral/' task '/data']; %Talapas path
 
@@ -48,7 +49,7 @@ for s = firstSub:lastSub
                     onsets{c} = run_info.onsets(currentIndices);
                     durations{c} = run_info.durations(currentIndices);
                 end
-                fxFolder = ['~/Desktop/REV_BxData/names_onsets_durations/' task '/'];
+                fxFolder = [repodir 'names_onsets_durations/' task '/'];
                 if exist(fxFolder)==7 %do nothing
                 else mkdir(fxFolder)
                 end    
