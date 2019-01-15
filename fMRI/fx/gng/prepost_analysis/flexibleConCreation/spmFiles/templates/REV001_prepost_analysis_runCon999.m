3e2af7026eaa7b420ad57ebd46cc5e01e48e9380
Krista DeStasio <kdestasio@gmail.com>
2019-01-15 11:08:44 -0800

    remove extra space in variable name "contrastNames"

--- fMRI/fx/gng/prepost_analysis/flexibleConCreation/spmFiles/templates/REV001_prepost_analysis_runCon999.m
+++ fMRI/fx/gng/prepost_analysis/flexibleConCreation/spmFiles/templates/REV001_prepost_analysis_runCon999.m
@@ -5,14 +5,14 @@
 %-----------------------------------------------------------------------
 matlabbatch{1}.cfg_basicio.var_ops.load_vars.matname = {'/projects/sanlab/shared/REV/REV_scripts/fMRI/fx/gng/prepost_analysis/flexibleConCreation/customCons/gng/prepost_analysis/customContrasts_sub-REV001_gng_prepost_analysis.mat'};
 matlabbatch{1}.cfg_basicio.var_ops.load_vars.loadvars.varname = {
-                                                                 ' contrastNames'
+                                                                 'contrastNames'
                                                                  'contrastCellArray'
                                                                  }';
 matlabbatch{2}.cfg_basicio.var_ops.subsrefvar.input(1) = cfg_dep('Load Variables from .mat File: Loaded Variable ''contrastCellArray''', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{2}));
 matlabbatch{2}.cfg_basicio.var_ops.subsrefvar.subsreference{1}.subsindc = {999};
 matlabbatch{2}.cfg_basicio.var_ops.subsrefvar.tgt_spec.r{1}.name = 'strtype';
 matlabbatch{2}.cfg_basicio.var_ops.subsrefvar.tgt_spec.r{1}.value = 'r';
-matlabbatch{3}.cfg_basicio.var_ops.subsrefvar.input(1) = cfg_dep('Load Variables from .mat File: Loaded Variable '' contrastNames''', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{1}));
+matlabbatch{3}.cfg_basicio.var_ops.subsrefvar.input(1) = cfg_dep('Load Variables from .mat File: Loaded Variable ''contrastNames''', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{1}));
 matlabbatch{3}.cfg_basicio.var_ops.subsrefvar.subsreference{1}.subsindc = {999};
 matlabbatch{3}.cfg_basicio.var_ops.subsrefvar.tgt_spec.s{1}.name = 'strtype';
 matlabbatch{3}.cfg_basicio.var_ops.subsrefvar.tgt_spec.s{1}.value = 's';
