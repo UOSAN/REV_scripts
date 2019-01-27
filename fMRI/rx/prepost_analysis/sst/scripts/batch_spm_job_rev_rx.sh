8fe652818c2b3380ec98e33a217d2af0d456a341
kdestasio <kdestasio@gmail.com>
2019-01-27 15:53:39 -0800

    include con_21 job

--- fMRI/rx/prepost_analysis/sst/scripts/batch_spm_job_rev_rx.sh
+++ fMRI/rx/prepost_analysis/sst/scripts/batch_spm_job_rev_rx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SCRIPTS=("rx_rev_con_06_ssrtCovariate.m", "rx_rev_con_07_ssrtCovariate.m")
-#("rx_rev_con_01.m", "rx_rev_con_02.m", "rx_rev_con_03.m", "rx_rev_con_04.m", "rx_rev_con_05.m", "rx_rev_con_06.m", "rx_rev_con_07.m", "rx_rev_con_08.m", "rx_rev_con_09.m", "rx_rev_con_10.m", "rx_rev_con_11.m", "rx_rev_con_12.m", "rx_rev_con_13.m", "rx_rev_con_14.m", "rx_rev_con_15.m", "rx_rev_con_16.m", "rx_rev_con_17.m", "rx_rev_con_18.m", "rx_rev_con_19.m", "rx_rev_con_20.m", "rx_rev_con_07_ssrtCovariate.m")
+SCRIPTS=("rx_rev_con_01.m", "rx_rev_con_02.m", "rx_rev_con_03.m", "rx_rev_con_04.m", "rx_rev_con_05.m", "rx_rev_con_06.m", "rx_rev_con_07.m", "rx_rev_con_08.m", "rx_rev_con_09.m", "rx_rev_con_10.m", "rx_rev_con_11.m", "rx_rev_con_12.m", "rx_rev_con_13.m", "rx_rev_con_14.m", "rx_rev_con_15.m", "rx_rev_con_16.m", "rx_rev_con_17.m", "rx_rev_con_18.m", "rx_rev_con_19.m", "rx_rev_con_20.m", "rx_rev_con_21.m", "rx_rev_con_07_ssrtCovariate.m", "rx_rev_con_21v09.m")
 SPM_PATH=/projects/sanlab/shared/spm12
 RESULTS_INFIX=sst
 
@@ -12,7 +11,9 @@ mempercpu=2G
 for SCRIPT in ${SCRIPTS[@]}; do
     if [[ $SCRIPT == *"Covariate"* ]]; then 
         SUBSTR=$(echo $SCRIPT | cut -c8-27)
-    else
+    elif [[ $SCRIPT == *con_21v09* ]]; then
+        SUBSTR=$(echo $SCRIPTS | cut -c8-16)
+    else 
         SUBSTR=$(echo $SCRIPT | cut -c8-13) 
     fi
     OUTPUTDIR=/projects/sanlab/shared/REV/REV_scripts/fMRI/rx/prepost_analysis/sst/${SUBSTR}
