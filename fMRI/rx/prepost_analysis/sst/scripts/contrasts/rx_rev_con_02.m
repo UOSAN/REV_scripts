6acabb81a692bb0b9c7cb274446bcf521bc73dde
Krista DeStasio <kdestasio@gmail.com>
2019-01-28 12:37:52 -0800

    split scripts into subdirectories

---
+++ fMRI/rx/prepost_analysis/sst/scripts/contrasts/rx_rev_con_02.m
@@ -0,0 +1,101 @@
+%-----------------------------------------------------------------------
+% Job saved on 02-Jan-2019 11:54:27 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/projects/sanlab/shared/REV/REV_scripts/fMRI/rx/prepost_analysis/sst/con_02'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV044/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV114/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV003/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV035/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV090/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV141/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV070/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV102/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV052/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV039/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV111/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV069/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV097/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV084/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV027/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV075/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV011/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV118/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV023/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV080/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV065/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV036/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV100/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV094/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV010/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV029/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV037/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV041/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV074/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV142/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV073/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV121/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV021/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV018/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV076/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV077/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV031/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV050/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV107/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV082/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV056/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV022/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV048/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV115/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV006/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV024/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV093/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV140/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV131/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV108/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV144/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV054/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV104/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV109/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV130/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV137/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV129/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV053/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV098/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV016/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV046/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV017/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV110/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV126/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV135/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV067/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV020/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV038/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV127/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV089/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV086/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV013/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV001/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV060/fx/sst/con_0002.nii',
+'/projects/sanlab/shared/REV/bids_data/derivatives/prepost_analysis/sub-REV078/fx/sst/con_0002.nii'
+};
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'CorrectStop>IncorrectStop';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'IncorrectStop>CorrectStop';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = -1;
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 0;
